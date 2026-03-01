31f7250606a9f19f03ad3ef14dd66c9383ebd759
GabrielSantana1996sp <gabrielsantanati@outlook.com>
2026-03-01 14:22:28 +0000

    Atualizar o Debian_Ubuntu-ServerMinimal__IceWM.sh

--- Debian_Ubuntu-ServerMinimal__IceWM.sh
+++ Debian_Ubuntu-ServerMinimal__IceWM.sh
@@ -36,14 +36,15 @@
 
 ## Script de Instalação
 
-```bash
 #!/bin/bash
 
 sudo apt update
 sudo apt install -y \
 icewm feh pasystray pavucontrol gsimplecal xfce4-notifyd gcalculator evince \
+pcmanfm lxterminal ristretto network-manager-gnome xfce4-power-manager clipit lxappearance papirus-icon-theme \
 python3-pip openjdk-21-jdk golang-go nodejs npm rustc cargo \
 git make cmake vim neovim geany \
 docker.io podman ansible kubectl terraform helm \
 ufw fail2ban auditd clamav apparmor \
-tmux htop neofetch ranger synaptic keepassxc joplin-desktop
+tmux htop neofetch ranger synaptic keepassxc joplin-desktop \
+p7zip-full unrar zip unzip catfish gnome-system-monitor gdebi mc lxrandr
